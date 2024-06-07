fc635a44657bbffb9727f42e010f79d4ce878f53
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-07 16:38:19 -0600

    Add spinnerArr.js code to spinner2.js, remove spinnerArr.js

--- spinner2.js
+++ spinner2.js
@@ -1,27 +1,24 @@
 // Spinner animation.
+// Extreme refactor. Unable to control how many spins the spinner completes, to increase or decrease spins one must alter the array.
 
-let revs = 2; // number of spins
-let revCount = 0;
 let delayTime = 100;
-let spinner = {
-  A: "\r|    ",
-  B: "\r/    ",
-  C: "\r-    ",
-  D: "\r\\    ",
-  E: "\r|    ",
-};
+let spinner = [
+  "\r|    ",
+  "\r/    ",
+  "\r-    ",
+  "\r\\    ",
+  "\r|    ",
+  "\r|    ",
+  "\r/    ",
+  "\r-    ",
+  "\r\\    ",
+  "\r|    ",
+  "\n",
+];
 
-while (revCount < revs) {
-  // Makes the spinner spin twice in this example
-  if (revCount === revs - 1) {
-    // During the last spin it adds a character to the object
-    spinner.F = "\n";
-  }
-  for (let frame in spinner) {
-    setTimeout(() => {
-      process.stdout.write(spinner[frame]);
-    }, delayTime);
-    delayTime += 200; // adds delay time
-  }
-  revCount++;
+for (let frame of spinner) {
+  setTimeout(() => {
+    process.stdout.write(frame);
+  }, delayTime);
+  delayTime += 200;
 }
